76b2fd3b6c860d88df297df6cdbefd677b665881
Nutzernam3 <sockeken@outlook.com>
2020-01-23 18:06:34 +0000

    initial commit

---
+++ Testscripts/Pic/convert_to_base64.py
@@ -0,0 +1,10 @@
+import base64
+
+#convert pic into base64
+encoded = base64.b64encode(open("test.png", "rb").read())
+print(encoded)
+
+#write pic.base64 into txt
+file = open("test.txt","w")
+file.write(repr(encoded))
+file.close()
