d9c74c7c71a70281cb16d49d8b78d41cdb37bdec
Nutzernam3 <kris.myslowski@koenig-bauer.com>
2020-01-30 09:14:20 +0100

    zusammenfassung

--- Testscripts/Pic/convert_to_jpg.py
+++ Testscripts/Pic/convert_to_jpg.py
@@ -4,11 +4,13 @@ import base64
 from PIL import Image
 from io import BytesIO
 
-f = open('test.txt', 'r')
-data = f.read()
-f.closed 
+def convert_to_jpg():
+    f = open('test.txt', 'r')
+    data = f.read()
+    f.closed
 
-im = Image.open(BytesIO(base64.b64decode(data)))
-im.save('newimage.jpg', 'JPEG')
+def save_to_jpg():
+    im = Image.open(BytesIO(base64.b64decode(data)))
+    im.save('newimage.jpg', 'JPEG')
 
-print("decoded to jpg...")
+    print("decoded to jpg...")
