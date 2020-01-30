8f5124c88a2c8043f883132b0726a04182abeafa
Nutzernam3 <kris.myslowski@koenig-bauer.com>
2020-01-30 08:08:13 +0100

    final + cosmetics

--- Testscripts/Pic/convert_to_jpg.py
+++ Testscripts/Pic/convert_to_jpg.py
@@ -1,16 +1,14 @@
+# -*- coding: utf-8 -*-
+
 import base64
 from PIL import Image
 from io import BytesIO
 
 f = open('test.txt', 'r')
 data = f.read()
-print(data)
 f.closed 
 
-# Soweit so gut
-# Daten werden eingelesen und ab Var übergeben 
-
-im = Image.open(BytesIO(base64.b64decode(data))) # Ich verstehe auch die Errormeldung nicht weil eig müsste die fkt funktionieren 
-
-
+im = Image.open(BytesIO(base64.b64decode(data)))
 im.save('newimage.jpg', 'JPEG')
+
+print("decoded to jpg...")
