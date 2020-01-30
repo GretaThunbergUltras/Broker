11b19522beffaa6920b3ca7d3bffee40824bf107
Nutzernam3 <kris.myslowski@koenig-bauer.com>
2020-01-30 08:06:55 +0100

    cosmetics

--- Testscripts/Pic/convert_to_base64.py
+++ Testscripts/Pic/convert_to_base64.py
@@ -2,9 +2,11 @@ import base64
 
 #convert pic into base64
 encoded = base64.b64encode(open("test.jpg", "rb").read())
-print(encoded)
+
 
 #write pic.base64 into txt
 file = open("test.txt","w")
 file.write(repr(encoded))
 file.close()
+
+print("encoded to b64...")
