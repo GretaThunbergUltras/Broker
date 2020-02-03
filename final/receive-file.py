f38734f1bc10200fcb54d86d4bd0ad5aea2d1fcc
Nutzername <sockeken@outlook.com>
2020-02-03 20:18:10 +0000

    new

--- final/receive-file.py
+++ final/receive-file.py
@@ -4,21 +4,28 @@ import time
 from botlib.broker import Broker
 
 # setup counter for images
-readNumber = open("number.txt").read()
-type(readNumber)
-number = int(readNumber)
-number = number + 1
+"""anzahl = open("number.txt").read()
+print(anzahl)
+type(anzahl)
+nummer=int(anzahl)
+nummer=nummer+1
+print(nummer)
+bennenung= str(nummer)
 
-file = open("number.txt", "w")
-file.write(NumberOfPic)
-file.close()
+file=open("number.txt","w")
+file.write(bennenung)
+file.close()"""
+
+
 
 # setup decoding and writing image
 def callback(cid, userdata, msg, ):
     print('received something')
     ##print(str(msg.payload))
     try:
-        with open('/media/extern/Images/image' + NumberOfPic + 'jpeg', 'wb') as out:
+        #with open('/media/extern/Images/image' + NumberOfPic + 'jpeg', 'wb') as out:
+        with open('test.jpeg', 'wb') as out:
+
             temp = str(msg.payload)[2:-1]
             ##print("Image saved "+str(len(temp)))
             temp2 = base64.b64decode(temp)
